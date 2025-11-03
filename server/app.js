cf75f821e89fadabf021105385c9b7ab17e8785c
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-11-03 10:20:37 -0500

    Added in require statements for app.js

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const helmet = require('helmet');
+
+const router = require('./router.js');
