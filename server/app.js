25b902b6d911cadf1d10b42f621961827c0b9dda
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-11-03 10:25:54 -0500

    Setup port and database URI

--- server/app.js
+++ server/app.js
@@ -7,3 +7,13 @@ const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 
 const router = require('./router.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+mongoose.connect(dbURI).catch((err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
+});
